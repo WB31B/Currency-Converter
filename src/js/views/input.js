1983711846891749c7de6e5953546f4eb9c37251
WB31B <pythontermux@gmail.com>
2022-02-21 23:21:43 +0200

    taking the index of an array of objects

--- src/js/views/input.js
+++ src/js/views/input.js
@@ -1,7 +1,7 @@
 import { select, btn, input } from "../variables/variables";
 import api from "../services/apiServices";
 
-export function btnClick() {
+function btnClick() {
     btn.addEventListener('click', e => {
         let inputData = input.value;
     
@@ -15,17 +15,19 @@ export function btnClick() {
     });
 }
 
-export function selectIndex() {
+function selectIndex() {
     select.addEventListener('change', e => {
         let index = select.selectedIndex;
-        
+
+        // Option has index and elements object have index
         api.currency().then(el => {
-            
+            const saleIndexOfOblect = el[index].sale; // Sale 
+            console.log(saleIndexOfOblect);
         })
     }); 
 }
 
 export function currencyConvert() {
     btnClick();
-    selectIndex();
+    selectIndex()
 }
